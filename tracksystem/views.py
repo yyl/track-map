e8ae9193367776eca06ed4a211616fea57391b56
yyl <yyl.dev@gmail.com>
2012-06-21 17:29:17 -0400

    add points on the map

--- tracksystem/views.py
+++ tracksystem/views.py
@@ -1,17 +1,23 @@
 from django.http import HttpResponse
 from models import Track
 from datetime import datetime
+from django.shortcuts import redirect
+from annoying.decorators import render_to
 
 # Create your views here.
 def query(request):
 	latitude_pass = request.GET.get('lat', None)
 	longitude_pass = request.GET.get('long', None)	
 	if latitude_pass and longitude_pass:
-		point = Track(
-					time=datetime.now().isoformat(' '), 
-					longitude = longitude_pass,
-					latitude = latitude_pass)
-		point.save()
-		return HttpResponse("Hello, world! "+ point.longitude)
+		Track(
+			time=datetime.now().isoformat(' '), 
+			longitude = longitude_pass,
+			latitude = latitude_pass
+			).save()
 	else:
-		return HttpResponse("Parameters are not in a correct form.")
+		return redirect('/')
+
+@render_to('map.html')
+def map(request):
+	points = Track.objects.all()  
+	return {'points': points}
