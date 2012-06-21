0c989a666852dc158b7ce4d895152d1057bdb5dc
yyl <yyl.dev@gmail.com>
2012-06-21 12:32:41 -0400

    keys file added

---
+++ myheroku/keys.py
@@ -0,0 +1,6 @@
+MY_DB_USER = 'djangodb'
+MY_DB_PASSWORD = '123'
+MY_STATIC_URL = 'https://s3.amazonaws.com/heroku-ynotserious/'
+MY_AWS_ACCESS_KEY_ID = 'AKIAJXYF4KB32T2RHF3A'
+MY_AWS_SECRET_ACCESS_KEY = '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'
+MY_AWS_STORAGE_BUCKET_NAME = 'heroku-ynotserious'
