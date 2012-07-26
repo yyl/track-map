d5cab8e0a3b8997aa06a58feca08ccb0ebef3e61
yyl <yyl.dev@gmail.com>
2012-07-26 11:02:43 -0400

    farEnough adds distance

--- tracksystem/views.py
+++ tracksystem/views.py
@@ -123,7 +123,7 @@ def handle_file_upload(f):
 		if match1 and match2 and matchtime:
 			lat = float(match2.group(1))
 			lon = float(match1.group(1))
-			if Track.objects.count() == 0 or farEnough(lat, lon, Track.objects.latest('time')):
+			if Track.objects.count() == 0 or farEnough(lat, lon, Track.objects.latest('time'), 23):
 				time = datetime.strptime(matchtime.group(1), "%Y-%m-%d-%H-%M-%S")
 				longitude = lon
 				latitude = lat
@@ -138,7 +138,7 @@ def handle_file_upload(f):
 	return output
 
 # return True if the point (lat, lon) is 10+ meters away from the track point
-def farEnough(lat, lon, track):
+def farEnough(lat, lon, track, distance):
 	degrees_to_radians = math.pi/180.0
 	
 	# phi = 90 - latitude
@@ -153,7 +153,7 @@ def farEnough(lat, lon, track):
 	       math.cos(phi1)*math.cos(phi2))
 	arc = math.acos(cos)
 	
-	return arc*6371*1000 > 23
+	return arc*6371*1000 > distance
 	
 ## given coordinates, do a google place search, return top 20 places matches the search
 def googlePlaces(lat, lon):
